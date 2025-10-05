a770c525cb7f2eb156cd38fe7cd7fdabf002fc31
andreaccamachoj <andreaccamachoj@gmail.com>
2025-10-05 13:31:02 -0500

    first commit

---
+++ applications/app-service/src/main/java/co/com/bb/kata/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.bb.kata.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.bb.kata.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
